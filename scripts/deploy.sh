a4a33f6d72a07d737ddddc54802f5e884120f569
Ricardo Ocampo <r.ocampo.vega@gmail.com>
2019-08-17 05:41:28 +0400

    Bugfix: Use setuptools instead of disutils to enable bdist_wheel command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   pip install --upgrade twine
-  python setup.py sdist
+  python setup.py sdist bdist_wheel
   python -m twine upload -u ${PYPI_USER} -p ${PYPI_PASSWORD} dist/*
 fi
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
-from distutils.core import setup
+
+from setuptools import setup
 
 import imquality
 
